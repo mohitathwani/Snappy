26abd383669c2f2f37d1f65a54a016686d844749
Mohit Athwani <mohit@athwani.net>
2019-02-02 14:47:28 -0800

    Initial Commit

---
+++ Snappy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snappy
+//
+//  Created by TeraMo Labs on 2/2/19.
+//  Copyright © 2019 TeraMo Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
