4d8c9e6960bf4a9f9df41cea1607cb6769303745
Mohit Athwani <mohit@athwani.net>
2019-03-09 14:59:00 -0800

    Media Manager is not multi threaded

--- Snappy/MediaManager/MediaManager.swift
+++ Snappy/MediaManager/MediaManager.swift
@@ -15,36 +15,47 @@ enum PhotoManagerStatus {
 }
 class MediaManager {
   
+  static let sessionQ = DispatchQueue(label: "media_manager_queue")
+  static let mainQ = DispatchQueue.main
+  
   static func verifyPermission(handler: @escaping (PhotoManagerStatus) ->Void) {
-    PHPhotoLibrary.requestAuthorization { (authorization) in
-      switch authorization {
-      case .authorized:
-        handler(.success)
-        
-      case .denied:
-        handler(.error(errorString: "Photo library access has been denied. Please update permission by going to the Settings->Privacy"))
-        
-      case .restricted:
-        handler(.error(errorString: "Photo library access has been restricted."))
-        
-      case .notDetermined:
-        fatalError("This should not have happened")
+    sessionQ.async {
+      PHPhotoLibrary.requestAuthorization { (authorization) in
+        mainQ.async {
+          switch authorization {
+          case .authorized:
+            handler(.success)
+            
+          case .denied:
+            handler(.error(errorString: "Photo library access has been denied. Please update permission by going to the Settings->Privacy"))
+            
+          case .restricted:
+            handler(.error(errorString: "Photo library access has been restricted."))
+            
+          case .notDetermined:
+            fatalError("This should not have happened")
+          }
+        }
       }
     }
   }
   
   static func savePhoto(data: Data, completion: @escaping (PhotoManagerStatus)->Void) {
-    PHPhotoLibrary.shared().performChanges({
-      let options = PHAssetResourceCreationOptions()
-      let creationRequest = PHAssetCreationRequest.forAsset()
-      creationRequest.addResource(with: .photo, data: data, options: options)
-      
-    }) { (success, error) in
-      if let error = error {
-        print(error.localizedDescription)
-        completion(.error(errorString: "Could not save image"))
+    sessionQ.async {
+      PHPhotoLibrary.shared().performChanges({
+        let options = PHAssetResourceCreationOptions()
+        let creationRequest = PHAssetCreationRequest.forAsset()
+        creationRequest.addResource(with: .photo, data: data, options: options)
+        
+      }) { (success, error) in
+        mainQ.async {
+          if let error = error {
+            print(error.localizedDescription)
+            completion(.error(errorString: "Could not save image"))
+          }
+          completion(.success)
+        }
       }
-      completion(.success)
     }
   }
 }
